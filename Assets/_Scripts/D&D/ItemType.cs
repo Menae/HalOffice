0f9bdc37c35151edcbbeb5352b216e30348ee2e7
Menae <131503202+Menae@users.noreply.github.com>
2026-04-16 23:41:54 +0900

    ファイルのリネーム、2日目分のオブジェクトの追加、配置

--- Assets/_Scripts/D&D/ItemType.cs
+++ Assets/_Scripts/D&D/ItemType.cs
@@ -6,27 +6,53 @@
 public enum ItemType
 {
     // ここにあなたのゲームに登場するアイテムの種類を追加していく
-    General,         // 一般的なもの（どのスロットにも置ける場合など）
-    SceneryA,       // 景色A
-    SceneryB,        // 景色B
-    Mannequin,      // マネキン
-    Books,           // 本
-    Chair,          // 椅子
-    Gramophone,    // 蓄音機
-    Mathematic,      // 数学道具
-    WorldMap,       //世界地図
-    Documents,      //散乱した書類
-    Aroma,         //アロマキャンドル
-    Banksy,        //バンクシーの絵
-    Headphone,      //ヘッドホン
-    Poster,         //ポスター
-    Drawings,        //スケッチ画
-    cosmetics,      //化粧品
-    GameConsole,   //ゲーム機
-    Stereo,         //ステレオ
-    TeddyBear,      //テディベア
-    Yogibo,        //ヨギボー
-    none,            //なし
-    AnimePoster,   //アニメポスター
+    General,            // 一般的なもの（どのスロットにも置ける場合など）
+    SceneryA,           // 景色A
+    SceneryB,           // 景色B
+    Mannequin,          // マネキン
+    Books,              // 本
+    Chair,              // 椅子
+    Gramophone,         // 蓄音機
+    Mathematic,         // 数学道具
+    WorldMap,           //世界地図
+    Documents,          //散乱した書類
+    Aroma,              //アロマキャンドル
+    Banksy,             //バンクシーの絵
+    Headphone,          //ヘッドホン
+    Poster,             //ポスター
+    Drawings,           //スケッチ画
+    cosmetics,          //化粧品
+    GameConsole,        //ゲーム機
+    Stereo,             //ステレオ
+    TeddyBear,          //テディベア
+    Yogibo,             //ヨギボー
+    none,               //なし
+    AnimePoster,        //アニメポスター
+    XXXXXXXXX,          // 区切り用（必要に応じて削除）
+    BLTV,               //BLTV
+    Cheki,              //チェキ
+    CupNoodle,          //カップヌードル
+    Cardboard1,         //段ボール1
+    Cardboard2,         //段ボール2
+    MoviePoster,        //映画ポスター
+    Trash1,              //ゴミ1
+    Trash2,             //ゴミ2
+    Trash3,             //ゴミ3
+    Comic,              //漫画
+    Clothes,             //服
+    Figure,             //フィギュア
+    Petbottle,           //ペットボトル
+    Bag,                 //バッグ
+    Drama,                //ドラマ
+    DogPortrait,          //犬の肖像画
+    Bag2,                //バッグ2
+    CuteClothes,           //かわいい服
+    Doll,                 //人形
+    HeelPoster,            //ヒールのポスター
+    Recipe,                //レシピ
+    Salad,                 //サラダ
+    Vacuum,                //掃除機
+    Plants,                //植物
+
 
 }
